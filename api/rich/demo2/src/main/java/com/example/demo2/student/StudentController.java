92e0834aa6c6d5b92346a2f23711e515b592ef9f
ahai <ahai@users.noreply.github.com>
2021-03-21 16:53:10 -0400

    outside the requestmapping

--- api/rich/demo2/src/main/java/com/example/demo2/student/StudentController.java
+++ api/rich/demo2/src/main/java/com/example/demo2/student/StudentController.java
@@ -4,13 +4,15 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping (path="api/v1/students")
 public class StudentController {
 
-    @RequestMapping (path="api/v1/students")
+    @GetMapping
     public List<Student> getStudent(){
         return List.of(
         		new Student(
