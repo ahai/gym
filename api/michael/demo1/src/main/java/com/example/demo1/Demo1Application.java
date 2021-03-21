14faee2b36d663741d07f97baa63bf87830dbba4
Pokemansanic <44726450+Pokemansanic@users.noreply.github.com>
2021-03-21 10:05:52 -0400

    boop

--- api/michael/demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ api/michael/demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -1,34 +1,13 @@
 package com.example.demo1;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-
 public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
 	
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-				new Student(
-						1L,
-						"Michael",
-						"mariam.jamal@gmail.com",
-						LocalDate.of(2000, Month.JANUARY, 5),
-						21
-					)
-		);
-	}
-
 }
