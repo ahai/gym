4bd33a437d0d23b84dd1073d200d49c4926316d9
Pokemansanic <44726450+Pokemansanic@users.noreply.github.com>
2021-04-07 20:57:46 -0400

    from initial to completed - spring batch.

--- api/michael/spring-guides-michael/15-gs-batch/gs-batch-processing-master/initial/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
+++ api/michael/spring-guides-michael/15-gs-batch/gs-batch-processing-master/initial/src/main/java/com/example/batchprocessing/BatchProcessingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BatchProcessingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchProcessingApplication.class, args);
-	}
-
+  public static void main(String[] args) throws Exception {
+    System.exit(SpringApplication.exit(SpringApplication.run(BatchProcessingApplication.class, args)));
+  }
 }
