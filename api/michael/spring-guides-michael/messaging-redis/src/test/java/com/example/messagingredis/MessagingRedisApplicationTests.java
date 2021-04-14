f25b7c8b55a7725845ce27b6afbe8ecae5245d3c
Pokemansanic <44726450+Pokemansanic@users.noreply.github.com>
2021-04-13 21:40:31 -0400

    clean up

---
+++ api/michael/spring-guides-michael/messaging-redis/src/test/java/com/example/messagingredis/MessagingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
