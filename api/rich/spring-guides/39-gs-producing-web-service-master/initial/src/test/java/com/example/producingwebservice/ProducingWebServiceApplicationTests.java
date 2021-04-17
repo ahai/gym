1377c75c1ad71e326256abf125bc3894bb77aac7
aDeveloper <ahai@users.noreply.github.com>
2021-04-16 21:15:08 -0400

    init 39 produce soap

---
+++ api/rich/spring-guides/39-gs-producing-web-service-master/initial/src/test/java/com/example/producingwebservice/ProducingWebServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.producingwebservice;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProducingWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
