ebb725fb16c39f91e8fca2380b4bf4ea66a8d776
Pokemansanic <44726450+Pokemansanic@users.noreply.github.com>
2021-04-17 21:36:49 -0400

    no 33

---
+++ api/michael/spring-guides-michael/33-gs-rest-service-cors-master/initial/src/test/java/com/example/restservicecors/RestServiceCorsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.restservicecors;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestServiceCorsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
