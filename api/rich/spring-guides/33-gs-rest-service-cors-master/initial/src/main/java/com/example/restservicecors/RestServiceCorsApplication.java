12267504cbcd177743bfe59e84a7e417c8432ee0
aDeveloper <ahai@users.noreply.github.com>
2021-04-17 21:49:07 -0400

    33 - cors

---
+++ api/rich/spring-guides/33-gs-rest-service-cors-master/initial/src/main/java/com/example/restservicecors/RestServiceCorsApplication.java
@@ -0,0 +1,24 @@
+package com.example.restservicecors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class RestServiceCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceCorsApplication.class, args);
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
+
+}
