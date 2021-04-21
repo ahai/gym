4ad0d7b08c0e1498039cfd55bb40693338848119
aDeveloper <ahai@users.noreply.github.com>
2021-04-20 21:19:36 -0400

    inital 53 restdocs

---
+++ api/rich/spring-guides/53-gs-testing-restdocs-master/initial/src/test/java/com/example/testingrestdocs/TestingRestdocsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.testingrestdocs;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingRestdocsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
