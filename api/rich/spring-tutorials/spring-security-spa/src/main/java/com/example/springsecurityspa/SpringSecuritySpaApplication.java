948373dcf550d4d7ffcf0e7ded6a3f691e16815e
aDeveloper <ahai@users.noreply.github.com>
2021-04-23 22:11:52 -0400

    clean up gateway and adding security+angular

---
+++ api/rich/spring-tutorials/spring-security-spa/src/main/java/com/example/springsecurityspa/SpringSecuritySpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySpaApplication.class, args);
+	}
+
+}
---
+++ api/rich/spring-tutorials/spring-security-spa/src/test/java/com/example/springsecurityspa/SpringSecuritySpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
