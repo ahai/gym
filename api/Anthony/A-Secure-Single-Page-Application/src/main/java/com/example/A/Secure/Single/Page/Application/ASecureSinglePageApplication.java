60f193c94b1650f279406366c209b797d01be553
ChampionOfElder <33139703+ChampionOfElder@users.noreply.github.com>
2021-04-24 20:46:05 -0400

    Update

---
+++ api/Anthony/A-Secure-Single-Page-Application/src/main/java/com/example/A/Secure/Single/Page/Application/ASecureSinglePageApplication.java
@@ -0,0 +1,13 @@
+package com.example.A.Secure.Single.Page.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASecureSinglePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ASecureSinglePageApplication.class, args);
+	}
+
+}
---
+++ api/Anthony/A-Secure-Single-Page-Application/src/test/java/com/example/A/Secure/Single/Page/Application/ASecureSinglePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.A.Secure.Single.Page.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASecureSinglePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
