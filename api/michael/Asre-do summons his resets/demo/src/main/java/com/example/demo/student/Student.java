28b8ba4e0849b5b1846a742918cfe20fbf656dc6
Pokemansanic <44726450+Pokemansanic@users.noreply.github.com>
2021-05-17 21:43:38 -0400

    Finish

--- api/michael/Asre-do summons his resets/demo/src/main/java/com/example/demo/student/Student.java
+++ api/michael/Asre-do summons his resets/demo/src/main/java/com/example/demo/student/Student.java
@@ -30,10 +30,10 @@ public class Student{
 	public Student() {
 	}
 
-    public Student(Long id,
-    		String name,
-    		String email,
-    		LocalDate dob) {
+	public Student(Long id,
+				   String name,
+				   String email,
+				   LocalDate dob) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
