b765702218f205765d209375532c2adeb8de9789
Pokemansanic <44726450+Pokemansanic@users.noreply.github.com>
2021-06-05 20:57:07 -0400

    done

--- api/michael/Asre-do summons his resets/Java Tutorial - Complete User Login and Registration Backend + Email Verification/demo/src/main/java/com/example/demo/email/EmailService.java
+++ api/michael/Asre-do summons his resets/Java Tutorial - Complete User Login and Registration Backend + Email Verification/demo/src/main/java/com/example/demo/email/EmailService.java
@@ -31,6 +31,7 @@ public class EmailService implements EmailSender{
             helper.setTo(to);
             helper.setSubject("Confirm your email");
             helper.setFrom("hello@amigoscode.com");
+            mailSender.send(mimeMessage);
         } catch (MessagingException e) {
             LOGGER.error("failed to send email", e);
             throw new IllegalStateException("failed to send email");
